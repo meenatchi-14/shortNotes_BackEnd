20051d52917518e700af9172ae38e9e96ae705c3
meenatchi <meenatchicse44@gmail.com>
2024-08-07 12:08:31 +0530

    console

--- routers/user.js
+++ routers/user.js
@@ -45,7 +45,7 @@ router.post("/signup",async(req,res)=>{
     const token=generateToken(user._id)
     res.status(201).json({data:"successfully register",token})         
     } catch (error) {
-        console.log("meena",error)
+        console.log("server-error",error)
         res.status(500).json({error:"interal server error"})
         
     }
